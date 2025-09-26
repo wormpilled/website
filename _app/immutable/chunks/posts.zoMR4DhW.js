e62079902c9fc1d097512fd27b855e43c2c190f1
wormpilled <wormpilled@users.noreply.github.com>
2025-09-25 18:13:10 -0700

    Deploy to gh-pages

---
+++ _app/immutable/chunks/posts.zoMR4DhW.js
@@ -0,0 +1,7 @@
+const e=[{slug:"new-vault-setup-(obsidian-blogging-cms)",htmlContent:`<p>This is your new <em>vault</em>. yes! you love to see it!</p>
+<p>Make a note of something, [[create a link]], or try <a href="https://help.obsidian.md/Plugins/Importer">the Importer</a>!</p>
+<p>When you&#39;re ready, delete this note and make the vault your own.</p>
+<p><img src="/UPLOADS/phone.jpg" alt=""></p>
+<p>another picture:
+<img src="/UPLOADS/troll.png" alt=""></p>
+`,title:"New Vault Setup",description:"Starter note with example images and links in Obsidian.",date:"2025-09-25T00:00:00.000Z",tags:["obsidian","fun"],draft:!1,cover:null}];export{e as p};
---
+++ _app/immutable/entry/start.brdX4Db4.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DKQ384CT.js";export{t as start};
